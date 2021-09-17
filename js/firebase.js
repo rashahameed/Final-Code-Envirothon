3fb5e610d082f6fd83d6cff1f232c2ae6b0f6930
Advaitvag <83405703+Advaitvag@users.noreply.github.com>
2021-09-17 21:05:09 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,34 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyC3Edi-zUQw6gocj89PJ8KVaB5vA6wPcek",
+    authDomain: "plantogram-a061d.firebaseapp.com",
+    projectId: "plantogram-a061d",
+    storageBucket: "plantogram-a061d.appspot.com",
+    messagingSenderId: "575424887011",
+    appId: "1:575424887011:web:beae853a40a2d151f7add6",
+    measurementId: "G-CMT6X5T51C"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+
+//signOut
+
+function signOut() {
+    auth.signOut();
+    alert("SignOut Successfully from System");
+}
+
+//active user to homepage
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        var email = user.email;
+        alert("Active user " + email);
+    } else {
+        alert("No active user");
+        window.location = 'signup.html';
+    }
+})
